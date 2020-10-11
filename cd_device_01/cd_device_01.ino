00dd35a22812f31a51141100f24c29df764479c5
sergevas <brighterr@gmail.com>
2020-10-12 01:03:54 +0300

    Implement the first version of the device

--- cd_device_01/cd_device_01.ino
+++ cd_device_01/cd_device_01.ino
@@ -1,21 +1,22 @@
-#include <ArduinoNATS.h>
-
-#include <SergeVas_dev_BME280.h>
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
-
+#include <ArduinoNATS.h>
+#include <SergeVas_dev_BME280.h>
 
 const int SDA_PIN = 4;  // D2 of WeMos D1 Mini
 const int SCL_PIN = 5; // D1 of WeMos D1 Mini
 
 const char* WIFI_SSID = "IoT";
-const char* WIFI_PASSWORD = "VeryL0ngPas$wd!2015";
+const char* WIFI_PASSWORD = "********";
 const int WIFI_NUM_OF_RETRIES = 20;
+const uint32_t NETWORK_ERROR_RECOVERY_DELAY = 600000000;
 
 char* NATS_SERVER_NAME="192.168.1.70";
 int NATS_SERVER_PORT = 4222;
-char* NATS_USER_NAME = "cgUser";
-char* NATS_PASSWORD = "cgPasswd";
+char* NATS_USER_NAME = "foo";
+char* NATS_PASSWORD = "secret";
+
+char* CG_STATUS_SUBJECT = "cg/status";
 
 BME280 rbfmiotBme280(I2C_ADDR_76);
 int8_t id;
@@ -24,11 +25,9 @@ char macAddr[12];
 WiFiClient client;
 NATS nats(
   &client,
-  NATS_SERVER_NAME,
-  NATS_DEFAULT_PORT,
-  NATS_USER_NAME,
-  NATS_PASSWORD
-);
+  "192.168.1.70",
+  NATS_DEFAULT_PORT
+  );
 
 String dToS(double aVal) {
   return String(aVal, 2);
@@ -38,12 +37,12 @@ String iToS(int8_t aVal) {
   return String(aVal, 2);
 }
 
-//char* sToC(String aVal) {
-//  int strValLength = strVal.length() + 1;
-//  char *strValArr = (char*)malloc(strValLength);
-//  strVal.toCharArray(strValArr, strValLength);
-//  return strValArr;
-//}
+char* sToC(String strVal) {
+  int strValLength = strVal.length() + 1;
+  char *strValArr = (char*)malloc(strValLength);
+  strVal.toCharArray(strValArr, strValLength);
+  return strValArr;
+}
 
 String readMACaddr() {
   int i;
@@ -79,11 +78,6 @@ void initWiFi() {
   Serial.println("WiFi connect complete...");
 }
 
-void nats_on_connect() {
-  String subject = "cg/" + readMACaddr();
-  nats.subscribe(subject, nats_handler);
-}
-
 void nats_handler(NATS::msg msg) {
   double temp, pres, hum;
   rbfmiotBme280.readAll(&temp, &pres, &hum);
@@ -93,8 +87,17 @@ void nats_handler(NATS::msg msg) {
     + ", \"pres\":" + dToS(pres)
     + ", \"hum\":" + dToS(hum) + "}";
     
-  nats.publish(msg.reply, replyMsg);
+  nats.publish(msg.reply, sToC(replyMsg));
 }
+
+void nats_on_connect() {
+  String macAddr = readMACaddr();
+  String subject = "cg/" + macAddr;
+  nats.subscribe(sToC(subject), nats_handler);
+  nats.publish(CG_STATUS_SUBJECT, sToC(subject));
+}
+
+
 
 void setup() {
   Serial.begin(115200);
@@ -104,13 +107,11 @@ void setup() {
   rbfmiotBme280.configure(SDA_PIN, SCL_PIN);
   Serial.println("Configure complete...");
   Serial.println("Reading device id start...");
-
   rbfmiotBme280.readId(&id);
   Serial.print("id 0x");
   Serial.println(id, HEX);
   Serial.println("Reading device id complete...");
-  readMACaddr(macAddr);
-  Serial.println(macAddr);
+  Serial.println(readMACaddr());
   nats.on_connect = nats_on_connect;
   nats.connect(); 
 }
